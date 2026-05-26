4b56bcd3bc43fe04f5e3d1f1f37756228507438a
dev <a@a.com>
2026-05-26 12:10:20 +0000

    fix: use HTTPS explorer domain instead of raw IP

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // Mainnet RPC bridge configuration.
 export const API_BASE = "https://api.hashlatch.online/api";
-export const EXPLORER_URL = "http://34.185.173.154:3001";
+export const EXPLORER_URL = "https://explorer.hashlatch.online";
 export const STRATUM_URL = "stratum+tcp://34.185.173.154:3052";
 export const STRATUM_HOST = "34.185.173.154";
 export const STRATUM_PORT = 3052;
